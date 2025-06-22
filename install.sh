9d0cc7bb6350f8ceef0c7e211a36943646c89e0b
hiro <hirohiro716@gmail.com>
2025-06-22 18:59:02 +0900

    Fix scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 directory=`dirname $0`
 json="com.hirohiro716.open_in_default_browser.json"
+mkdir -p ~/.mozilla/native-messaging-hosts/
 cp "$directory/native-messaging-hosts/$json" ~/.mozilla/native-messaging-hosts/
 sed -i "s/YOUR_USERNAME/$USER/g" ~/.mozilla/native-messaging-hosts/$json
 pythonScript="com.hirohiro716.open_in_default_browser.py"
